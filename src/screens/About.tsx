d30e82aec973c571a321ab008f0344ed1e19fea0
sk97kang <sk97.kang@gmail.com>
2020-11-10 16:14:35 +0900

    Add About

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { Typography } from "antd";
+const { Title } = Typography;
 function About() {
-  return <div>About</div>;
+  return (
+    <Typography>
+      <Title>Komflix</Title>
+    </Typography>
+  );
 }
 
 export default About;
